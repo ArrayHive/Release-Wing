d9077082e4e0485c44a407639dfdf6005db0c64e
Hannik <33319800+Yunaido@users.noreply.github.com>
2024-07-12 14:37:42 +0000

    feat: init backend with spring initializer

---
+++ backend/src/main/java/dev/arrayhive/release_wing/ReleaseWingApplication.java
@@ -0,0 +1,13 @@
+package dev.arrayhive.release_wing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseWingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseWingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/arrayhive/release_wing/ReleaseWingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arrayhive.release_wing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseWingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
